374eb17bd996c1049e5ffd0c4c21e5413bf95149
ninjasujan <sujan@appyhigh.com>
2021-10-01 21:57:36 +0530

    commiting before experiment on migration script

--- Models/tweetModel.js
+++ Models/tweetModel.js
@@ -2,27 +2,27 @@ const Sequelize = require("sequelize");
 const sequelize = require("../db/connection");
 
 const Tweet = sequelize.define(
-  "tweets",
-  {
-    _id: {
-      type: Sequelize.INTEGER(11),
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    content: Sequelize.STRING(300),
-  },
-  {
-    timestamps: true,
-  }
+	"tweets",
+	{
+		_id: {
+			type: Sequelize.INTEGER(11),
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		content: Sequelize.STRING(300),
+	},
+	{
+		timestamps: true,
+	}
 );
 
 Tweet.associate = (models) => {
-  Tweet.belongsTo(models.users);
+	Tweet.belongsTo(models.users);
 };
 
 Tweet.associate = (models) => {
-  Tweet.belongsToMany(models.tags, { through: "tweet_tag" });
+	Tweet.belongsToMany(models.tags, { through: "tweet_tag" });
 };
 
-module.exports = tweets;
+module.exports = Tweet;
--- Models/userModel.js
+++ Models/userModel.js
@@ -2,35 +2,35 @@ const Sequelize = require("sequelize");
 const sequelize = require("../db/connection");
 
 const User = sequelize.define(
-  "users",
-  {
-    _id: {
-      type: Sequelize.INTEGER(11),
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    username: {
-      type: Sequelize.STRING(35),
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: Sequelize.STRING(20),
-      allowNull: false,
-    },
-  },
-  {
-    timestamps: true,
-  }
+	"users",
+	{
+		_id: {
+			type: Sequelize.INTEGER(11),
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		username: {
+			type: Sequelize.STRING(35),
+			allowNull: false,
+			unique: true,
+		},
+		password: {
+			type: Sequelize.STRING(20),
+			allowNull: false,
+		},
+	},
+	{
+		timestamps: true,
+	}
 );
 
 User.associate = (models) => {
-  User.hasMany(models.tweets);
+	User.hasMany(models.tweets);
 };
 
 User.associate = (models) => {
-  User.hasOne(models.subscriptions);
+	User.hasOne(models.subscriptions);
 };
 
 module.exports = User;
